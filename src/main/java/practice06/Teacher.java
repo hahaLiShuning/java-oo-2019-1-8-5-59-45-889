d8058432c5b28cd377245f3ce51c8dd79726ed40
LIShuning <lishiuning1955142623@outlook.com>
2019-06-11 19:03:37 +0800

    Practice06 completed

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,25 @@
 package practice06;
 
-public class Teacher {
+import javax.annotation.Nullable;
+
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age, int... klass) {
+        super(name, age);
+        if (klass.length > 0) {
+            this.klass = klass[0];
+        }
+    }
+
+    public int getKlass() {
+        return this.klass;
+    }
+
+    @Override
+    public String introduce() {
+        String ret = super.introduce().concat(" I am a Teacher. I teach");
+        ret.concat(this.klass > 0 ? String.format("Class %d", this.klass) : "No Class");
+        return ret;
+    }
 }
